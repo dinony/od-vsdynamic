e200ef5b92af5af517aaaaae0d9bd36268670214
dinony <onur.dogangoenuel@gmail.com>
2017-04-26 17:56:28 +0200

    Update browserSync

--- tools/browserSync.js
+++ tools/browserSync.js
@@ -6,7 +6,7 @@ browserSync({
   logFileChanges: true,
   reloadDelay: 200,
   reloadDebounce: 500,
-  files: ['src/**/*.ts', 'index-dev.html', 'systemjs.config.js'],
+  files: ['src/**/*.ts', 'src/**/*.css', 'index-dev.html', 'systemjs.config.js'],
   watchOptions: {ignored: 'node_modules'},
   server: {baseDir: './',directory: true}
 });
